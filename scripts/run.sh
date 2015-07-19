c8386050bf8e184b4f73baa77a2889f175829714
Kureev Alexey <kureev-mail@ya.ru>
2015-07-19 21:45:06 +0200

    Add sleep to shell script to be sure bundle file for testing has been created

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir -p dist
-node src/server dist/bundle.js &
 ./node_modules/.bin/watchify example/*.js -o dist/bundle.js &
 ./node_modules/.bin/http-server example &
+node src/server dist/bundle.js &
+sleep 3
 open http://localhost:8080
 wait
