15fce0876f4cb539dd3b9f14f8d6ee16fa3a21c8
Kureev Alexey <kureev-mail@ya.ru>
2015-07-24 15:45:53 +0200

    Prevent sending empty bundle patch

--- server/makePatch.js
+++ server/makePatch.js
@@ -2,6 +2,8 @@ const jsdiff = require('diff');
 
 module.exports = function scopeOriginal(original) {
   return function calculateDiff(changed) {
+    if (!changed.length) return false;
+
     return jsdiff.createPatch(Date.now(), original, changed);
   };
 };
--- server/notify.js
+++ server/notify.js
@@ -1,5 +1,7 @@
 module.exports = function createBoradcast(wss) {
   return function broadcast(patch) {
+    if (!patch) return false;
+
     wss.clients.forEach(function each(client) {
       client.send(JSON.stringify({ patch: patch, }));
     });
