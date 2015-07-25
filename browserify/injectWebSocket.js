c17726be3a60f7b33a09f951edb5a92d582d2c74
Kureev Alexey <kureev-mail@ya.ru>
2015-07-25 16:20:56 +0200

    Neat messages in browser console

--- browserify/injectWebSocket.js
+++ browserify/injectWebSocket.js
@@ -1,3 +1,12 @@
+var moment = require('moment');
+var Logdown = require('logdown');
+var logger = new Logdown({ prefix: '[BDS]', });
+var diff = require('diff');
+var size = 0;
+var patched;
+var timestamp;
+var data;
+
 function bytesToKb(bytes) {
   return Math.round((bytes / 1024) * 100) / 100;
 }
@@ -7,21 +16,19 @@ module.exports = function injectWebSocket(scope) {
   scope.ws = new WebSocket('ws://localhost:8081');
 
   scope.ws.onmessage = function onMessage(res) {
-    var diff = require('diff');
-    var size = 0;
-    var patched;
-    var data = JSON.parse(res.data);
+    timestamp = '['+ moment().format('HH:mm:ss') + ']';
+    data = JSON.parse(res.data);
 
     if (data.source) {
       scope.initialBundle = data.source;
       size = bytesToKb(data.source.length);
-      console.log('Initial bundle size:', size, 'kb');
+      logger.log(timestamp + ' Initial bundle size: *' + size + 'kb*');
     }
 
     if (data.patch) {
       patched = diff.applyPatch(scope.initialBundle, data.patch);
       size = bytesToKb(data.patch.length);
-      console.log('Received patch for ', size, 'kb');
+      logger.log(timestamp + ' Received patch for *' + size + 'kb*');
       Function('return ' + patched)();
     }
 
