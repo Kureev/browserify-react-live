1fe5c891ad4bb88dc83db0e692952b662a4a4082
Kureev Alexey <kureev-mail@ya.ru>
2015-08-05 17:47:36 +0200

    Support react/addons

--- src/injectReactDeps.js
+++ src/injectReactDeps.js
@@ -1,5 +1,6 @@
 module.exports = function injectReactDeps(scope) {
   scope.React = require('react');
+  scope.ReactAddons = require('react/addons');
   scope.ReactMount = require('react/lib/ReactMount');
   scope.makeHot = require('react-hot-api')(
     function getRootInstances() {
--- src/overrideRequire.js
+++ src/overrideRequire.js
@@ -1,6 +1,6 @@
 function isReloadable(name) {
-  // @todo Replace this sketch by normal one
-  return name.indexOf('react') === -1;
+  var isReact = ['react', 'react/addons', 'react-dom', ].indexOf(name) > -1;
+  return !isReact;
 }
 
 module.exports = function overrideRequire(scope, req) {
@@ -10,6 +10,8 @@ module.exports = function overrideRequire(scope, req) {
         return scope.React;
       } else if (name === 'react-dom') {
         return scope.ReactDOM;
+      } else if (name === 'react/addons') {
+        return scope.ReactAddons;
       }
     } else {
       scope.modules = scope.modules || {};
