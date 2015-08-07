0278d5bd2ffcd2d28ece680342615de48087a117
Kureev Alexey <kureev-mail@ya.ru>
2015-08-07 17:35:18 +0200

    Fix bug with requiring module with the same name(s) in different folder(s)

--- src/overrideRequire.js
+++ src/overrideRequire.js
@@ -1,12 +1,15 @@
+var path = require('path');
+
 function isReloadable(name) {
   var isReact = ['react', 'react/addons', 'react-dom', ].indexOf(name) > -1;
   return !isReact;
 }
 
-module.exports = function overrideRequire(scope, req) {
+module.exports = function overrideRequire(scope, req, filename) {
   scope.modules = scope.modules || [];
 
   return function overrideRequire(name) {
+    var __name;
     if (!isReloadable(name)) {
       if (name === 'react') {
         return scope.React;
@@ -16,11 +19,13 @@ module.exports = function overrideRequire(scope, req) {
         return scope.ReactAddons;
       }
     } else {
-      if (scope.modules[name]) {
-        return scope.modules[name];
+      __name = path.resolve(filename, '../', name);
+
+      if (scope.modules[__name]) {
+        return scope.modules[__name];
       }
-      scope.modules[name] = req(name);
-      return scope.modules[name];
+      scope.modules[__name] = req(name);
+      return scope.modules[__name];
     }
   };
 };
