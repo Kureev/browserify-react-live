9e8784277b6a41a8ef222cc12186a3a73f4f3937
Kureev Alexey <kureev-mail@ya.ru>
2015-08-09 02:57:25 +0200

    Some clean-ups in applyPatch

--- src/actions/applyPatch.js
+++ src/actions/applyPatch.js
@@ -2,32 +2,60 @@ var moment = require('moment');
 var bytesToKb = require('./bytesToKb');
 var diff = require('diff');
 
-module.exports = function applyPatch(scope, source, data) {
-  var timestamp = '['+ moment().format('HH:mm:ss') + ']';
-
-  console.groupCollapsed(timestamp, 'Patch for',
-    data.file, '(' + bytesToKb(data.patch.length) + 'kb)');
-
-  var patchArr = data.patch.split('\n');
-  patchArr.forEach(function iteratePatch(str) {
+/**
+ * Parse patch file and log changes in a fancy way
+ * @param  {String} patch
+ * @return {Void}
+ */
+function logChanges(patch) {
+  patch.split('\n').forEach(function iteratePatch(str) {
     if (str.match(/^\+\s/)) {
       console.log('%c' + str.replace(/^\+\s+/, '+ '), 'color: green');
     } else if (str.match(/^\-\s/)) {
       console.log('%c' + str.replace(/^-\s+/, '- '), 'color: red');
     }
   });
+}
+
+/**
+ * Check if react hot replacement can
+ * be applied to the module
+ * @param  {Object} module
+ * @return {Boolean}
+ */
+function canBePatched(module) {
+  return module.exports &&
+    module.exports.name ||
+    module.exports.displayName;
+}
+
+module.exports = function applyPatch(scope, source, data) {
+  var timestamp = '['+ moment().format('HH:mm:ss') + ']';
+
+  console.groupCollapsed(timestamp, 'Patch for',
+    data.file, '(' + bytesToKb(data.patch.length) + 'kb)');
+
+  logChanges(data.patch);
 
   var patched = diff.applyPatch(source, data.patch);
 
+  // Build full file path for require
   var filename = scope.__root + '/' + data.file;
+
+  // Building require for specific filename
+  // In our custom require we'll use filename to scope relative paths
   var __require = require('../overrideRequire')(scope, require, filename);
+
+  // Create anon function for our patched source
   var f = Function(['require', 'module', 'exports', ], patched);
 
+  // Mock module to get function result
   var _module = {};
+
+  // Run module like as browserify does
   f(__require, _module, {});
 
-  if (_module.exports &&
-    (_module.exports.name || _module.exports.displayName)) {
+  if (canBePatched(_module)) {
     scope.makeHot(_module.exports);
   }
 
