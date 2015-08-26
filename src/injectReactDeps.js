7f2f899cb12d1495f64687fc71653c39747d9d73
Kureev Alexey <kureev-mail@ya.ru>
2015-08-26 13:57:52 +0200

    Remove makeHot from scope

--- src/injectReactDeps.js
+++ src/injectReactDeps.js
@@ -2,9 +2,4 @@ module.exports = function injectReactDeps(scope) {
   scope.React = require('react');
   scope.ReactAddons = require('react/addons');
   scope.ReactMount = require('react/lib/ReactMount');
-  scope.makeHot = require('react-hot-api')(
-    function getRootInstances() {
-      return scope.ReactMount._instancesByReactRootID;
-    }
-  );
 };
